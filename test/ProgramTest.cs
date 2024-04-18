365260b3dcac2f10acd8dc739b123aa7ab78932d
Doina Plesca <doinitzaplesca@yahoo.com>
2024-04-18 08:38:02 +0200

    program test

---
+++ test/ProgramTest.cs
@@ -0,0 +1,23 @@
+using System.Reflection;
+using api.Helper;
+using infrastructure.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Playwright;
+using NUnit.Framework;
+using service;
+
+namespace test;
+
+public class ProgramTest
+{
+    [Test]
+    public void Run_DoesNotThrowException()
+    {
+        // Arrange
+        var program = new Program();
+
+        // Act & Assert
+        Assert.DoesNotThrow(() => program.Run(null));
+    }
+
+}
