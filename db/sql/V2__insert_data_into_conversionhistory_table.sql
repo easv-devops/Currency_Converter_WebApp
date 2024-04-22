714f224a239d3d565342f08e7cbc90b34b9c7a34
Doina Plesca <doinitzaplesca@yahoo.com>
2024-04-22 11:05:28 +0200

    update docker-compose

---
+++ db/sql/V2__insert_data_into_conversionhistory_table.sql
@@ -0,0 +1,6 @@
+INSERT INTO conversionhistory (sourcecurrency, targetcurrency, amount, convertedamount, timestamp)
+VALUES ('USD', 'EUR', 100, 85.50, NOW()),
+       ('EUR', 'GBP', 200, 170.20, NOW()),
+       ('GBP', 'USD', 150, 200.10, NOW()),
+       ('USD', 'EUR', 100, 93, NOW()),
+       ('USD', 'EUR', 200, 74.4, NOW());
--- tests/e2e.js
+++ tests/e2e.js
@@ -1,34 +1,33 @@
-﻿
-import { Selector } from 'testcafe';
+﻿import {Selector} from 'testcafe';
 
 fixture`HistoryController End-to-End Test`
-    .page`http://localhost:5000`; 
+    .page`http://localhost:5000`;
 
 test('Should fetch conversion history successfully', async t => {
-  
+
     await t
         .navigateTo('/conversion/history');
     await t
         .expect(Selector('status-code').innerText).eql('200');
     await t
         .expect(Selector('response-message').innerText).contains('Conversion history fetched successfully');
-    
+
     const responseData = await Selector('response-data').innerText;
     const responseJson = JSON.parse(responseData);
     const conversionHistory = responseJson.responseData;
-    
+
     await t
         .expect(conversionHistory.length).gt(0, 'At least one record should exist in the conversion history');
 
     for (const record of conversionHistory) {
         await t
-            .expect(record).contains({ 
+            .expect(record).contains({
                 sourceCurrency: 'USD',
                 targetCurrency: 'EUR',
                 amount: 100,
                 convertedAmount: 93.00
             });
-        
+
     }
 });
 
